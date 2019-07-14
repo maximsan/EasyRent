27885575afbf98d72f63a6da06929ad9dcb49e6c
maximsan <maximuzz@yandex.ru>
2019-07-15 00:14:16 +0300

    Add input group component

--- EasyRent.Server/wwwroot/src/components/filter/Filter.jsx
+++ EasyRent.Server/wwwroot/src/components/filter/Filter.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../button/Button';
 import Select from '../select/Select';
 import CheckBoxGroup from '../checkbox/CheckBoxGroup';
+import InputGroup from '../inputGroup/InputGroup';
 import customClasses from './Filter.module.css';
 
 const Filter = ({}) => {
+  const [price, setPrice] = useState({ priceFrom: 0, priceTo: 0 });
+
+  const onChange = (event) => {
+    event.persist();
+    setPrice((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   return (
     <aside className={customClasses.filter}>
       <Select>Select a category</Select>
@@ -13,7 +24,28 @@ const Filter = ({}) => {
       <Select>Select district</Select>
       <hr />
       <CheckBoxGroup items={[{ label: 'free' }, { label: 'paid' }]} />
-      price from to days form to with photos last 5 days all ads last month
+      <InputGroup
+        label='Price'
+        items={[
+          {
+            placeholder: 'from',
+            name: 'priceFrom',
+            value: price.priceFrom,
+            onChange,
+          },
+          {
+            placeholder: 'to',
+            name: 'priceTo',
+            value: price.priceTo,
+            onChange,
+          },
+        ]}
+      />
+      <InputGroup
+        label='Days'
+        items={[{ placeholder: 'from' }, { placeholder: 'to' }]}
+      />
+      with photos last 5 days all ads last month
       <div>
         <Button>Show</Button>
         <Button>Clear</Button>

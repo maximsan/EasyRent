76220c8cb77c38d437586dcc488cc071961fc6b6
maximsan <maximuzz@yandex.ru>
2019-07-19 22:45:20 +0300

    Add photo api to test masonry

--- EasyRent.Server/wwwroot/src/components/main/Main.jsx
+++ EasyRent.Server/wwwroot/src/components/main/Main.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid, makeStyles } from '@material-ui/core';
+import axios from 'axios';
 import Header from '../header/Header';
 import Filter from '../filter/Filter';
 import Masonry from '../masonry/Masonry';
@@ -10,14 +11,31 @@ const useClasses = makeStyles({
   },
 });
 
+// Set config defaults when creating the instance
+const instance = axios.create({
+  baseURL: 'https://api.unsplash.com/',
+});
+
 const Main = () => {
   const classes = useClasses();
+  const [images, setImages] = useState(null);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const { data } = await instance({
+        method: 'get',
+        url: `photos/?client_id=${process.env.ACCESS_KEY}`,
+      });
+      setImages(data);
+    };
+    fetchImages();
+  }, []);
 
   return (
     <Grid container classes={{ root: classes.root }}>
       <Header />
       <Filter />
-      <Masonry />
+      <Masonry images={images} />
     </Grid>
   );
 };
--- EasyRent.Server/wwwroot/src/components/masonry/Masonry.jsx
+++ EasyRent.Server/wwwroot/src/components/masonry/Masonry.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MasonryComponent from 'react-masonry-component';
 import classes from './Masonry.module.css';
 
 const Masonry = ({ images }) => {
   const imgElements =
     images &&
-    images.map(({ src }) => (
-      <div className={classes.Img}>
-        <img src={src} alt='' />
+    images.slice(0, 20).map(({ title, url, id }) => (
+      <div className={classes.Img} key={id}>
+        <img src={url} alt='' title={title} />
       </div>
     ));
 
   return <MasonryComponent>{imgElements}</MasonryComponent>;
 };
+
+Masonry.propTypes = {};
+Masonry.defaultProps = {};
 
 export default Masonry;

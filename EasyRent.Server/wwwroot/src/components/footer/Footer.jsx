1bcaec825f1147d5b5ffd16c745ce606f2ad9863
maximsan <maximuzz@yandex.ru>
2019-07-20 20:50:20 +0300

    Add footer

---
+++ EasyRent.Server/wwwroot/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>Make a donation</div>
+      <div>Contact with us</div>
+    </footer>
+  );
+};
+
+export default Footer;

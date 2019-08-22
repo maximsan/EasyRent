4733304db9aabc77cdc9425cec0d8f3e85d7680d
maximsan <maximuzz@yandex.ru>
2019-08-22 22:54:36 +0300

    Create add ad icon

--- EasyRent.Server/wwwroot/src/components/icons/AddIcon.jsx
+++ EasyRent.Server/wwwroot/src/components/icons/AddIcon.jsx
@@ -3,24 +3,24 @@ import PropTypes from 'prop-types';
 import Icon from './Icon';
 import customClasses from './icon.module.css';
 
-const BookmarksIcon = () => {
+const AddIcon = () => {
   return (
     <Icon
       className={customClasses.fabRoot}
       href=''
       size='medium'
       fontSize='large'
-      titleAccess='Go to bookmarks'
+      titleAccess='Add advertisement'
       viewBox='0 0 24 24'
     >
       <path fill='none' d='M0 0h24v24H0V0z' />
-      <path d='M15 7v12.97l-4.21-1.81-.79-.34-.79.34L5 19.97V7h10m4-6H8.99C7.89 1 7 1.9 7 3h10c1.1 0 2 .9 2 2v13l2 1V3c0-1.1-.9-2-2-2zm-4 4H5c-1.1 0-2 .9-2 2v16l7-3 7 3V7c0-1.1-.9-2-2-2z' />
+      <path d='M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z' />
     </Icon>
   );
 };
 
-BookmarksIcon.defaultTypes = {};
+AddIcon.defaultTypes = {};
 
-BookmarksIcon.propTypes = {};
+AddIcon.propTypes = {};
 
-export default BookmarksIcon;
+export default AddIcon;

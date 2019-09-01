ee52690f3e249e0b56d651a6d627f29313bb2648
maximsan <maximuzz@yandex.ru>
2019-09-01 13:27:30 +0300

    Adjust side-bar btn styles

--- EasyRent.Server/wwwroot/src/components/common/button/SideBarButton.jsx
+++ EasyRent.Server/wwwroot/src/components/common/button/SideBarButton.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { IconButton } from '@material-ui/core';
+import { IconButton, makeStyles } from '@material-ui/core';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import styles from './button.module.css';
 
+const useClasses = makeStyles({
+  root: {
+    color: '#086574',
+    fontSize: '2rem',
+  },
+});
+
 const SideBarButton = ({ extraClasses, open, ...rest }) => {
+  const { root } = useClasses();
+
   return (
     <IconButton {...rest} classes={{ ...extraClasses }}>
-      {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+      {open ? (
+        <ChevronLeftIcon classes={{ root }} />
+      ) : (
+        <ChevronRightIcon classes={{ root }} />
+      )}
     </IconButton>
   );
 };

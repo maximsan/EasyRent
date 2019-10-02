ccb77fa876d6a00a35daebc092606ad9fd5b0325
maximsan <maximuzz@yandex.ru>
2019-10-03 00:06:39 +0300

    start setting up sign in

--- EasyRent.Server/wwwroot/src/reducers/actionTypes.jsx
+++ EasyRent.Server/wwwroot/src/reducers/actionTypes.jsx
@@ -3,3 +3,4 @@ export const SIGN_UP = 'SIGN_UP';
 export const START_REQUEST = 'START_REQUEST';
 export const STOP_REQUEST = 'STOP_REQUEST';
 export const RECEIVED_DATA = 'RECEIVED_DATA';
+export const HANDLE_ERROR = 'HANDLE_ERROR';

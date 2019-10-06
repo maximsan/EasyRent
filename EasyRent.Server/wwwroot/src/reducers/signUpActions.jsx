bae1c6d159f7065290a4a80aa557f65c3c828ac2
maximsan <maximuzz@yandex.ru>
2019-10-06 17:02:50 +0300

    update actions

--- EasyRent.Server/wwwroot/src/reducers/signUpActions.jsx
+++ EasyRent.Server/wwwroot/src/reducers/signUpActions.jsx
@@ -1,3 +1,12 @@
+import { SIGN_UP_SUCCESS, SIGN_UP } from './actionTypes';
+
+export const signUp = (data) => {
+  return {
+    type: SIGN_UP,
+    data,
+  };
+};
+
 export const signUpSuccess = (data) => {
   return {
     type: SIGN_UP_SUCCESS,

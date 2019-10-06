62224f9514f9bf443b6534cde5d03b0db5564ebb
maximsan <maximuzz@yandex.ru>
2019-10-06 09:25:13 +0300

    update action types

--- EasyRent.Server/wwwroot/src/reducers/actionTypes.jsx
+++ EasyRent.Server/wwwroot/src/reducers/actionTypes.jsx
@@ -1,6 +1,10 @@
 export const SIGN_IN = 'SIGN IN';
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
 export const SIGN_UP = 'SIGN_UP';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+
 export const START_REQUEST = 'START_REQUEST';
 export const STOP_REQUEST = 'STOP_REQUEST';
-export const RECEIVED_DATA = 'RECEIVED_DATA';
 export const HANDLE_ERROR = 'HANDLE_ERROR';
+
+export const RECEIVED_DATA = 'RECEIVED_DATA';

337789bb4f6c469578bbaf50dd1a4f561349743f
Konstantin <33716293+PushMeNow@users.noreply.github.com>
2020-07-26 21:03:00 +0300

    Correlate Ad and Admodel

--- EasyRent.BusinessLayer/Models/AdModels/AdModel.cs
+++ EasyRent.BusinessLayer/Models/AdModels/AdModel.cs
@@ -1,14 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EasyRent.BusinessLayer.Models.AdModels
 {
     public class AdModel
     {
         public int AdId { get; set; }
+
         public ICollection<ImageModel> Images { get; set; }
+
+        [MaxLength(500)]
         public string Description { get; set; }
+
+        [MaxLength(100)]
         public string Location { get; set; }
+
         public decimal Price { get; set; }
+
+        [MaxLength(50)]
         public string Title { get; set; }
+
+        public int Available { get; set; }
+
+        public DateTime CreatedDate { get; set; }
     }
 }
